ff57092bbffb5f46ec3fb9ab4189352e5644d047
dongsuseo <poopoo0515@naver.com>
2025-04-15 17:06:39 +0900

    Chore: "폴더 구조 정리"

---
+++ front/app/layout.tsx
@@ -0,0 +1,14 @@
+import './globals.scss';
+import './reset.css';
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="ko">
+            <body>{children}</body>
+        </html>
+    );
+}
---
+++ front/app/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+export default function Home() {
+    return (
+        <div>
+            <div className={styles.base}>기본! 123</div>
+            <div className={styles.toss}>토스! 123</div>
+        </div>
+    );
+}
