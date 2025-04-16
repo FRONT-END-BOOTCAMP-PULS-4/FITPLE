d416314ee6641e014bc952cb0d4e691b0130225e
서유덕 <awesome36@daum.net>
2025-04-16 16:32:53 +0900

    Feat : SkillBadge 컴포넌트 추가 및 스타일 적용, 기술스택 SVG 이미지 추가

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,10 +1,12 @@
-import styles from "./page.module.scss";
+'use client';
+
+import styles from './page.module.scss';
 
 export default function Home() {
-  return (
-    <div>
-      <div className={styles.base}>기본! 123</div>
-      <div className={styles.toss}>토스! 123</div>
-    </div>
-  );
+    return (
+        <div>
+            <div className={styles.base}>기본! 123</div>
+            <div className={styles.toss}>토스! 123</div>
+        </div>
+    );
 }
--- front/components/Badge/SkillBadge.tsx
+++ front/components/Badge/SkillBadge.tsx
@@ -1,7 +1,21 @@
-const SkillBadge = () => {
-    return (
-        <div>
-            <span></span>
+import styles from './SkillBadge.module.scss';
+import Image from 'next/image';
+
+type Props = {
+    type?: 'icon' | 'label';
+    name: string;
+    label?: string;
+};
+
+const SkillBadge = (props: Props) => {
+    const { type = 'label', name, label, ...rest } = props;
+
+    return type === 'icon' ? (
+        <Image src={`/images/${name}.svg`} width={30} height={30} alt={name} className={styles.skillLogo} {...rest} />
+    ) : (
+        <div className={styles.label} {...rest}>
+            <Image src={`/images/${name}.svg`} width={45} height={45} alt={name} className={styles.skillLogo} />
+            <div>{label ? label : name}</div>
         </div>
     );
 };
