caaee9d8455fb7ba59b61f23c262d963177e7e9c
Hyunwoo-Seo <shw1801@naver.com>
2025-04-17 14:34:14 +0900

    Feat: 공용 컴포넌트 MobileFooter 구현

--- fitple/app/page.tsx
+++ fitple/app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import styles from './page.module.scss';
+import MobileFooter from '../components/MobileFooter/MobileFooter';
 
 export default function Home() {
     return (
         <div>
+            <MobileFooter />
             <div className={styles.base}>기본! 123</div>
             <div className={styles.toss}>토스! 123</div>
         </div>
