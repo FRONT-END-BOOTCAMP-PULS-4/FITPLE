0cd64b3415f0accedac99e15d1ef552d7711b19b
Hyunwoo-Seo <shw1801@naver.com>
2025-04-17 14:39:54 +0900

    Fix: 루트 페이지에 테스트용 컴포넌트 삭제

--- fitple/app/page.tsx
+++ fitple/app/page.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import styles from './page.module.scss';
-import MobileFooter from '../components/MobileFooter/MobileFooter';
 
 export default function Home() {
     return (
-        <div>
-            <MobileFooter />
+        <div>ㄴ
             <div className={styles.base}>기본! 123</div>
             <div className={styles.toss}>토스! 123</div>
         </div>
