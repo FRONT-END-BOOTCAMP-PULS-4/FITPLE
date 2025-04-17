411c5186b38b33ff5c232923f822b25a888c3e5d
Harang <shw1801@naver.com>
2025-04-17 14:42:42 +0900

    Fix: 오타 수정

--- fitple/app/page.tsx
+++ fitple/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.scss';
 
 export default function Home() {
     return (
-        <div>ㄴ
+        <div>
             <div className={styles.base}>기본! 123</div>
             <div className={styles.toss}>토스! 123</div>
         </div>
