5aa48de5e6fcb98f7a093cc318c95434d3325861
서유덕 <awesome36@daum.net>
2025-04-22 10:52:44 +0900

    Feat : project, introduction 게시글 작성 페이지 UI 작업

---
+++ fitple/app/board/project/components/ProjectForm.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import styles from './ProjectForm.module.scss';
+import { useForm } from 'react-hook-form';
+import { RecruitmentStatus, WorkMode } from '@/type/common';
+import Label from '@/components/Input/Label';
+import SelectBox from '@/components/Select/SelectBox';
+import { SKILLS, SkillOption, POSITIONS, PositionOption, SkillValue, PositionValue } from '@/constants';
+import Textarea from '@/components/Textarea/Textarea';
+import Button from '@/components/Button/Button';
+import Input from '@/components/Input/Input';
+import SelectWorkMode from '../../components/SelectWorkMode';
+import SelectStatus from '../../components/SelectStatus';
+import SelectDuration from '../../components/SelectDuration';
+import SelectImages from '../../components/SelectImages';
+
+export type ProjectFormData = {
+    title: string;
+    content: string;
+    positions: PositionValue[];
+    skills: SkillValue[];
+    workMode: WorkMode;
+    status: RecruitmentStatus;
+    duration: number;
+    imgFile: File | null;
+};
+
+/** 여기서 수정이라면, 기존 데이터 가져와서 보여주기 */
+
+const ProjectForm = () => {
+    const { register, handleSubmit, setValue, watch } = useForm<ProjectFormData>({
+        mode: 'onSubmit',
+    });
+    const selectedPositions = watch('positions') || [];
+    const selectedSkills = watch('skills') || [];
+
+    const onSubmit = (data: ProjectFormData) => {
+        console.log('폼 제출 값:', data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.layout}>
+            <Input variant="underline" placeholder="제목을 입력해주세요" {...register('title', { required: true })} />
+
+            <div className={styles.selectBox}>
+                <SelectWorkMode register={register} setValue={setValue} />
+                <SelectStatus register={register} setValue={setValue} />
+                <SelectDuration register={register} setValue={setValue} />
+
+                <Label label="모집포지션" direction="row">
+                    <SelectBox
+                        {...register('positions', { required: true })}
+                        options={POSITIONS as unknown as PositionOption[]}
+                        selectedValues={selectedPositions}
+                        onChange={(value) => setValue('positions', value)}
+                        placeholder="포지션 선택"
+                    />
+                </Label>
+
+                <Label label="기술 스택" direction="row">
+                    <SelectBox
+                        {...register('skills', { required: true })}
+                        options={SKILLS as unknown as SkillOption[]}
+                        selectedValues={selectedSkills}
+                        onChange={(value) => setValue('skills', value)}
+                        placeholder="기술 선택"
+                        maxSelected={5}
+                    />
+                </Label>
+            </div>
+
+            <Textarea
+                {...register('content', { required: '프로젝트 소개를 작성해주세요' })}
+                onChange={(e) => setValue('content', e.target.value)}
+                size="md"
+                placeholder={`# 프로젝트 소개 예시
+                - 어떤 프로젝트인가요?
+                - 사용할 기술 스택은?
+                - 대략적인 프로젝트 기간은 어떻게 되나요?
+                자유롭게 작성해 주세요 :)`}
+            />
+
+            <SelectImages label="사진을 넣어주세요" onChange={(file) => setValue('imgFile', file)} />
+
+            <div className={styles.btnBox}>
+                <Button variant="cancel" size="md">
+                    취소
+                </Button>
+                <Button variant="confirm" type="submit" size="md">
+                    확인
+                </Button>
+            </div>
+        </form>
+    );
+};
+
+export default ProjectForm;
--- fitple/app/page.tsx
+++ fitple/app/page.tsx
@@ -1,100 +1,12 @@
 'use client';
 
-import { useEffect } from 'react';
 import styles from './page.module.scss';
 
 export default function Home() {
-    useEffect(() => {
-        const fetchProjectDetail = async () => {
-            const response = await fetch('/api/projects/1');
-            const data = await response.json();
-
-            console.log('프로젝트 상세', data);
-        };
-
-        const fetchProjectList = async () => {
-            const response = await fetch('/api/projects');
-            const data = await response.json();
-
-            console.log('프로젝트 리스트', data);
-        };
-
-        fetchProjectDetail();
-        fetchProjectList();
-    }, []);
-
-    const handlePostProject = async () => {
-        const fakeData = {
-            userId: 'c1fd1600-a222-4c01-a8c9-94d09267b8e5',
-            title: '테스트 입니다',
-            content: '테스트 내용입니다',
-            duration: 1,
-            workMode: 'online',
-            status: 'open',
-            skillIds: [1, 2],
-            imgUrls: [
-                '{https://image.utoimage.com/preview/cp872722/2022/12/202212008462_500.jpg, https://media.istockphoto.com/id/157373207/ko/%EC%82%AC%EC%A7%84/%EA%B7%A0%ED%98%95-%EC%9E%A1%ED%9E%8C-%EA%B2%B0%EC%84%9D-%EC%9E%88%EB%8A%94-%ED%8E%98%EB%B8%94-%EB%B9%84%EC%B9%98-%EC%A4%91-%ED%95%B4%EC%A7%88%EB%85%98%EA%B9%8C%EC%A7%80.jpg?s=612x612&w=0&k=20&c=Ry2S96sojRl2jUnX-XAE5pD8qyj9FQSQnsCEUT_B0pU=}',
-            ],
-            positionIds: [3],
-        };
-
-        const response = await fetch('/api/member/projects', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(fakeData),
-        });
-
-        const responseData = await response.json();
-        console.log('프로젝트 게시글 생성', responseData);
-    };
-
-    const handleUpdateProject = async () => {
-        const fakeData = {
-            userId: 'c1fd1600-a222-4c01-a8c9-94d09267b8e5',
-            id: 18,
-            title: '제목을 바꿔봅시다',
-            content: '내용이 바뀌어야 정상입니다.',
-            duration: 1,
-            workMode: 'offline',
-            status: 'open',
-            positionIds: [3],
-            skillIds: [3],
-            imgUrls: ['{https://image.utoimage.com/preview/cp872722/2022/12/202212008462_500.jpg}'],
-        };
-
-        const response = await fetch('/api/member/projects/18', {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(fakeData),
-        });
-
-        const responseData = await response.json();
-        console.log('프로젝트 게시글 수정', responseData);
-    };
-
-    const handlerDeleteProject = async () => {
-        const response = await fetch('/api/member/projects/11', {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-
-        const responseData = await response.json();
-        console.log('프로젝트 삭제', responseData);
-    };
-
     return (
         <div>
             <div className={styles.base}>기본! 123</div>
             <div className={styles.toss}>토스! 123</div>
-            <button onClick={handlePostProject}>post 하기</button>
-            <button onClick={handleUpdateProject}>put 하기</button>
-            <button onClick={handlerDeleteProject}>delete 하기</button>
         </div>
     );
 }
