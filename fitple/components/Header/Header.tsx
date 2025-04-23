c10c937cf68455d88b36aae9b3f795985b60cc5f
Hyunwoo-Seo <shw1801@naver.com>
2025-04-23 10:28:16 +0900

    Fix: Header에서 로그인 상태 확인하여 UI 변경하게 수정

--- fitple/components/Header/Header.tsx
+++ fitple/components/Header/Header.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import styles from './Header.module.scss';
 import Image from 'next/image';
+import { useAuthStore } from '@/stores/authStore';
+import Link from 'next/link';
 
 const Header = () => {
+    const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+    const nickname = useAuthStore((state) => state.nickname);
+
+    console.log(nickname)
+
     return (
         <div className={styles.headerContainer}>
             <Image
@@ -9,10 +18,23 @@ const Header = () => {
                 alt="logo"
                 width={120}
                 height={50}
-             />
+            />
             <ul className={styles.ulStyles}>
-                <li><div className={`${styles.userIcon}`} /></li>
-                <li><div className={`${styles.notificationIcon}`} /></li>
+                {isAuthenticated() ? (
+                    <>
+                        <li className={styles.welcomeText}>
+                            {nickname}님 환영합니다!
+                        </li>
+                        <Link href="/mypage">
+                            <li><div className={styles.userIcon} /></li>
+                        </Link>
+                        <li><div className={styles.notificationIcon} /></li>
+                    </>
+                ) : (
+                    <Link href="/login">
+                        <li className={styles.loginText}>로그인</li>
+                    </Link>
+                )}
             </ul>
         </div>
     );
