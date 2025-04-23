0da53fd837387310b70cbe9a952a1c7fd83027a5
roast <roastt1@naver.com>
2025-04-24 01:12:34 +0900

    Fix: 상세페이지 작성

--- fitple/app/page.tsx
+++ fitple/app/page.tsx
@@ -1,36 +1,37 @@
-'use client';
+"use client";
 
 // import EmblaCarousel from 'embla-carousel';
 // import styles from './page.module.scss';
-import PopularCarousel from '@/components/Carousel/PopularCarousel/PopularCarousel';
-import { EmblaOptionsType } from 'embla-carousel';
-import Card from '@/components/Card/Card';
-import { Tab } from '@/components/Tab/Tab';
-import FloatButton from '@/components/FloatButton/FloatButton';
-import { introductionPosts, projectPosts } from '@/constants/posts';
-import { useRouter } from 'next/navigation';
-import Badge from '@/components/Badge/Badge';
-import styles from './page.module.scss';
-import SkillBadge from '@/components/Badge/SkillBadge';
-import { useEffect, useState } from 'react';
+import PopularCarousel from "@/components/Carousel/PopularCarousel/PopularCarousel";
+import { EmblaOptionsType } from "embla-carousel";
+import Card from "@/components/Card/Card";
+import { Tab } from "@/components/Tab/Tab";
+import FloatButton from "@/components/FloatButton/FloatButton";
+import { introductionPosts, projectPosts } from "@/constants/posts";
+import { useRouter } from "next/navigation";
+import Badge from "@/components/Badge/Badge";
+import styles from "./page.module.scss";
+import SkillBadge from "@/components/Badge/SkillBadge";
+import { useEffect, useState } from "react";
+import { ProjectListDto } from "@/back/project/application/usecases/dto/ProjectListDto";
 export default function Home() {
     const router = useRouter();
     const OPTIONS: EmblaOptionsType = {
         // 필요한 Embla 옵션들을 정의
     };
     const badgeColor = {
-        project: 'var(--brand-color)',
-        introduction: 'var(--lion-color)',
+        project: "var(--brand-color)",
+        introduction: "var(--lion-color)",
     } as const;
     // const { data, error } =
     // await supabase.from('project').select('*').order('likes', { ascending: false }).limit(5); 근데 project랑 introduction 다 가져와서 해야됨;
 
     async function fetchProjects() {
         try {
-            const res = await fetch('/api/projects', {
-                method: 'GET',
+            const res = await fetch("/api/projects", {
+                method: "GET",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                 },
             });
 
@@ -41,11 +42,11 @@ export default function Home() {
             const data = await res.json();
             return data;
         } catch (error) {
-            console.error('프로젝트 데이터를 불러오는 중 오류 발생:', error);
+            console.error("프로젝트 데이터를 불러오는 중 오류 발생:", error);
             return null;
         }
     }
-    const [projects, setProjects] = useState<any[]>([]);
+    const [projects, setProjects] = useState<ProjectListDto[]>([]);
 
     useEffect(() => {
         const getProjects = async () => {
@@ -58,7 +59,7 @@ export default function Home() {
     }, []);
     const popularPosts = projects.sort((a, b) => b.likeCount - a.likeCount).slice(0, 5); //나중에 인트로덕션도해야됨
     const cardsArray = popularPosts.map((post) => (
-        <div key={post.id} onClick={() => router.push(`/${post.type}/${post.id}`)} style={{ cursor: 'pointer' }}>
+        <div key={post.id} onClick={() => router.push(`/${post.type}/${post.id}`)} style={{ cursor: "pointer" }}>
             {/* <Card
                 header={
                     <div>
