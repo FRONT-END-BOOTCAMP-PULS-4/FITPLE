ed44986a36fee8d8fc82efd8b73e74a18f6722d1
roast <roastt1@naver.com>
2025-04-25 15:03:35 +0900

    Fix: 카드 컴포넌트 size props 추가

--- fitple/components/Card/Card.tsx
+++ fitple/components/Card/Card.tsx
@@ -1,15 +1,16 @@
-import React, { FC, ReactNode } from "react";
-import styles from "./Card.module.scss";
+import React, { FC, ReactNode } from 'react';
+import styles from './Card.module.scss';
 
 type CardProps = {
     header?: ReactNode; // 상단 커스터마이징
     body: ReactNode; // 본문 커스터마이징
     footer?: ReactNode; // 하단 커스터마이징
+    size?: 'sm' | 'md' | 'lg'; // 버튼 크기
 };
 
-const Card: FC<CardProps> = ({ header, body, footer, ...props }) => {
+const Card: FC<CardProps> = ({ header, body, footer, size = 'md', ...props }) => {
     return (
-        <div className={styles.card} {...props}>
+        <div className={`${styles.card} ${styles[size]}`} {...props}>
             {/* 상단: header는 있으면 렌더링 */}
             <div className={styles.cardHeader}>{header}</div>
 
