166cad7ef9d763aec7c6673e986bcab5f7b987c7
dongsuseo <poopoo0515@naver.com>
2025-04-25 21:31:49 +0900

    Fix: googleWrapper를 빼먹어서 추가 커밋합니다

--- fitple/app/layout.tsx
+++ fitple/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.scss';
 import './reset.css';
 import { DeviceLayoutProvider } from '@/providers/DeviceLayoutProvider';
 import DefaultLayout from '@/components/Layout/DefaultLayout';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 export default function RootLayout({
     children,
@@ -11,11 +12,12 @@ export default function RootLayout({
     return (
         <html>
             <body>
-                <DeviceLayoutProvider>
-                    <DefaultLayout>{children}</DefaultLayout>
-                </DeviceLayoutProvider>
+                <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+                    <DeviceLayoutProvider>
+                        <DefaultLayout>{children}</DefaultLayout>
+                    </DeviceLayoutProvider>
+                </GoogleOAuthProvider>
             </body>
         </html>
-
     );
 }
