9c570ac906ae1ec904cbc696b80a54aa66ff5af9
서유덕 <awesome36@daum.net>
2025-04-26 17:47:21 +0900

    Fix : DefaultLayout 에서 로그인 페이지 시에 Header, Footer 안보이도록 수정 완료

--- fitple/components/Layout/DefaultLayout.tsx
+++ fitple/components/Layout/DefaultLayout.tsx
@@ -1,6 +1,10 @@
+'use client';
+
+import styles from './DefaultLayout.module.scss';
 import { ReactNode } from 'react';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import { usePathname } from 'next/navigation';
 
 type DefaultLayoutProps = {
     hideFooter?: boolean;
@@ -10,23 +14,27 @@ type DefaultLayoutProps = {
 
 const DefaultLayout: React.FC<DefaultLayoutProps> = (props) => {
     const { hideFooter = false, hideHeader = false, children } = props;
+    const pathname = usePathname();
+
+    const isLoginPath = pathname?.startsWith('/login');
 
     const FOOTER_HEIGHT = '223px';
-    const GNB_HEIGHT = '133px';
+    const HEADER_HEIGHT = '133px';
 
     return (
         <>
-            {!hideHeader && <Header />}
+            {!hideHeader && !isLoginPath && <Header />}
             <main
+                className={isLoginPath ? styles.loginLayout : ''}
                 style={{
-                    minHeight: `calc(100vh${!hideHeader ? ` - ${GNB_HEIGHT}` : 0}${
+                    minHeight: `calc(100vh${!hideHeader ? ` - ${HEADER_HEIGHT}` : 0}${
                         !hideFooter ? ` - ${FOOTER_HEIGHT}` : 0
                     })`,
                 }}
             >
                 {children}
             </main>
-            {!hideFooter && <Footer />}
+            {!hideFooter && !isLoginPath && <Footer />}
         </>
     );
 };
