e0d719426f641ba4b648d7da4a79ff82a6bebaf1
dongsuseo <poopoo0515@naver.com>
2025-04-28 11:14:25 +0900

    Style: 기타 스타일 수정하였습니다

--- fitple/components/Banner/Banner.tsx
+++ fitple/components/Banner/Banner.tsx
@@ -3,20 +3,20 @@ import { Carousel } from 'antd';
 import styles from './Banner.module.scss';
 
 const Banner: React.FC = () => (
-  <Carousel autoplay autoplaySpeed={2000} className={styles.banner}>
-    <div className={styles.imageBox}>
-      <img src='/images/banner1.png' />
-    </div>
-    <div className={styles.imageBox}>
-      <img src='/images/banner2.png' />
-    </div>
-    <div className={styles.imageBox}>
-      <img src='/images/banner3.png' />
-    </div>
-    <div className={styles.imageBox}>
-      <img src='/images/banner4.png' />
-    </div>
-  </Carousel>
+    <Carousel autoplay autoplaySpeed={2000} className={styles.banner}>
+        {/* <div className={styles.imageBox}>
+            <img src="/images/banner1.png" />
+        </div>
+        <div className={styles.imageBox}>
+            <img src="/images/sunset.jpg" className={styles.img} />
+        </div>
+        <div className={styles.imageBox}>
+            <img src="/images/butterfly.jpg" className={styles.img} />
+        </div> */}
+        <div className={styles.imageBox}>
+            <img src="/images/banner4.png" />
+        </div>
+    </Carousel>
 );
 
 export default Banner;
