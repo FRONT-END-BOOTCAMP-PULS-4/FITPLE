650ce967c9f7924ff810e65442bdea5da11909a9
dongsuseo <poopoo0515@naver.com>
2025-04-28 11:13:05 +0900

    Fix: 이미지 경로 못불러오던 오류 수정

--- fitple/next.config.ts
+++ fitple/next.config.ts
@@ -1,48 +1,55 @@
-import type { NextConfig } from "next";
- 
+import type { NextConfig } from 'next';
+
 const nextConfig: NextConfig = {
-  // TurboPack 설정
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
-  // webpack 설정
-  webpack: (config) => {
-    // @ts-expect-error 타입 에러 무시
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg')
-    );
- 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, 
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              typescript: true,
-              ext: 'tsx',
+    // TurboPack 설정
+    experimental: {
+        turbo: {
+            rules: {
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.js',
+                },
             },
-          },
-        ],
-      }
-    );
-    fileLoaderRule.exclude = /\.svg$/i;
-    return config;
-  },
+        },
+    },
+    images: {
+        domains: [
+            'img1.kakaocdn.net',
+            't1.kakaocdn.net',
+            'k.kakaocdn.net',
+            'lh3.googleusercontent.com',
+            'img1.googleusercontent.com',
+        ], // 필요한 다른 도메인도 같이 추가 가능
+    },
+    // webpack 설정
+    webpack: (config) => {
+        // @ts-expect-error 타입 에러 무시
+        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/,
+            },
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+                use: [
+                    {
+                        loader: '@svgr/webpack',
+                        options: {
+                            typescript: true,
+                            ext: 'tsx',
+                        },
+                    },
+                ],
+            }
+        );
+        fileLoaderRule.exclude = /\.svg$/i;
+        return config;
+    },
 };
- 
+
 export default nextConfig;
